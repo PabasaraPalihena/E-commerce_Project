9e169d9a03fddee850863c750e95aa2631bfa81c
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-21 03:02:12 +0530

    setup tsconfig

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@
 //   reactStrictMode: true,
 // }
 
+const { withFrameworkConfig } = require("./framework/common/config");
+
 // module.exports = nextConfig
-module.exports = {
+module.exports = withFrameworkConfig({
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US",
   },
-};
+});
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2));
