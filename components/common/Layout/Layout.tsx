8da23157c1631243f40ec46ddb19557b6a6683d4
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-21 04:19:10 +0530

    added layout component

---
+++ components/common/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+
+interface LayoutProps {
+  children?: React.ReactNode
+}
+
+const Layout: FC<LayoutProps> = ({children}) => {
+  return (
+    <div className="layout">
+      { children }
+    </div>
+  )
+}
+
+export default Layout
+
