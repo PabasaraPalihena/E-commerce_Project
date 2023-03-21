04799f351fa05ccc38c74733e0986589dfa6e9aa
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-21 09:28:46 +0530

    added layout styling

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 
 import React,{ FC } from "react"
+import s from "./Layout.module.css"
 
 interface Props {
     children: React.ReactNode;
@@ -7,9 +8,11 @@ interface Props {
 
 const Layout: FC<Props> = ({ children })  => {
   return (
-    <div className="layout">
+    <>
+    <main className={s.root}>
       { children }
-    </div>
+    </main>
+  </>
   )
 }
 
