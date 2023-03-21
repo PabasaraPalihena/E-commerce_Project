2347109ffa59cfa9685691e0712101d64a2ea18f
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-21 14:10:29 +0530

    image scaling issue

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -28,7 +28,6 @@ const ProductCard: FC<Props> = ({product}) => {
             height={540}
             width={540}
             quality="85"
-            layout="responsive"
             className={s.productImg}
           />
         )
@@ -39,38 +38,3 @@ const ProductCard: FC<Props> = ({product}) => {
 }
 
 export default ProductCard
-
-// import { Product } from "@common/types/product"
-// import { FC } from "react"
-// import Link from "next/link"
-// import Image from "next/image"
-// import s from "./ProductCard.module.css"
-// interface Props {
-//   product: Product
-// }
-// const placeholderImage = "/product-image-placeholder.svg"
-// const ProductCard: FC<Props> = ({product}) => {
-//   return (
-//     <Link href={`/products/${product.slug}`} className={s.root}>
-//         <div className={s.productBg}></div>
-//         <div className={s.productTag}>
-//           <h3 className={s.productTitle}>
-//             <span>{product.name}</span>
-//           </h3>
-//           <span className={s.productPrice}>14 $</span>
-//         </div>
-//         { product.images && (
-//           <Image
-//             alt={product.name ?? "Product image"}
-//             src={product.images[0].url ?? placeholderImage}
-//             height={540}
-//             width={540}
-//             quality="85"
-//             layout="responsive"
-//           />
-//         )
-//         }
-//     </Link>
-//   )
-// }
-// export default ProductCard
