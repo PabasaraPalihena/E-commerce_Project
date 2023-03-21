e08a0ba16551e8123f5f09d3c93572c4cabad09c
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-21 13:49:42 +0530

    set the bg colour

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from "@common/types/product"
 import { FC } from "react"
 import Link from "next/link"
 import Image from "next/image"
+import s from "./ProductCard.module.css"
 
 interface Props {
   product: Product
@@ -11,13 +12,14 @@ const placeholderImage = "/product-image-placeholder.svg"
 const ProductCard: FC<Props> = ({product}) => {
 
   return (
-    <Link href={`/products/${product.slug}`}>
-      
-        <div>
-          <h3>
+    <Link href={`/products/${product.slug}`} legacyBehavior>
+      <a className={s.root}>
+      <div className={s.productBg}></div>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>14 $</span>
+          <span className={s.productPrice}>14 $</span>
         </div>
         { product.images && (
           <Image
@@ -27,12 +29,48 @@ const ProductCard: FC<Props> = ({product}) => {
             width={540}
             quality="85"
             layout="responsive"
+            className={s.productImg}
           />
         )
         }
-
+        </a>
     </Link>
   )
 }
 
 export default ProductCard
+
+// import { Product } from "@common/types/product"
+// import { FC } from "react"
+// import Link from "next/link"
+// import Image from "next/image"
+// import s from "./ProductCard.module.css"
+// interface Props {
+//   product: Product
+// }
+// const placeholderImage = "/product-image-placeholder.svg"
+// const ProductCard: FC<Props> = ({product}) => {
+//   return (
+//     <Link href={`/products/${product.slug}`} className={s.root}>
+//         <div className={s.productBg}></div>
+//         <div className={s.productTag}>
+//           <h3 className={s.productTitle}>
+//             <span>{product.name}</span>
+//           </h3>
+//           <span className={s.productPrice}>14 $</span>
+//         </div>
+//         { product.images && (
+//           <Image
+//             alt={product.name ?? "Product image"}
+//             src={product.images[0].url ?? placeholderImage}
+//             height={540}
+//             width={540}
+//             quality="85"
+//             layout="responsive"
+//           />
+//         )
+//         }
+//     </Link>
+//   )
+// }
+// export default ProductCard
