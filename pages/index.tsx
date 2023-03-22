eea5d8ef121858894e0fc1d24a5f1b8457b0c8c3
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-22 13:06:23 +0530

    created layout b

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,6 +42,14 @@ export default function Home({products}:InferGetServerSidePropsType<typeof getSt
           />
         )}
       </Marquee>
+      <Grid layout="B">
+        { products.slice(0,3).map(product =>
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+      )}
+      </Grid>
       </>
   )
 }
