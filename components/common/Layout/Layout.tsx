5c92241ddcb69de1162cf7afdf37f0af4b3399dd
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-22 14:40:24 +0530

    added the sidebar and whishlist

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -2,6 +2,8 @@
 import React,{ FC } from "react"
 import s from "./Layout.module.css"
 import { Footer,Navbar } from "@components/common"
+import { Sidebar } from "@components/ui"
+import { CartSidebar } from "@components/cart"
 
 interface Props {
     children: React.ReactNode;
@@ -11,6 +13,9 @@ const Layout: FC<Props> = ({ children })  => {
   return (
     <div className={s.root}>
        <Navbar />
+       <Sidebar>
+        <CartSidebar />
+      </Sidebar>
     <main className="fit">
       { children }
     </main>
