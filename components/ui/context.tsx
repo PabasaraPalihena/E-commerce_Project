092eb865965fcfefff4f2427bee2c928d1abe44f
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-22 16:08:48 +0530

    completed the sidebar

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,4 +1,9 @@
-import { createContext, FC, useContext,useReducer } from "react"
+import {
+  createContext,
+  FC,
+  useContext,
+  useReducer,
+  useMemo } from "react"
 
 interface Props {
   children: React.ReactNode;
@@ -54,11 +59,13 @@ export const UIProvider: FC<Props> = ({children}) => {
   const openSidebar = () => dispatch({type: "OPEN_SIDEBAR"})
   const closeSidebar = () => dispatch({type: "CLOSE_SIDEBAR"})
 
-  const value = {
-    ...state,
-    openSidebar,
-    closeSidebar,
-  }
+  const value = useMemo(() => {
+    return {
+      ...state,
+      openSidebar,
+      closeSidebar
+    }
+  }, [state.isSidebarOpen])
 
   return (
     <UIContext.Provider value={value}>
