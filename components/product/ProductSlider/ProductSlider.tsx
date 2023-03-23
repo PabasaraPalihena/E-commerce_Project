0bd5965e7f38e4c0bf523244e0bfbd7b47003a1a
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-23 12:03:28 +0530

    fetched the product images

---
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+import s from "./ProductSlider.module.css"
+
+interface Props {
+    children: React.ReactNode;
+  }
+
+const ProductSlider: FC<Props> = ({children}) => {
+
+  return (
+    <div className={s.root}>
+      <div className="h-full transition-opacity">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default ProductSlider
