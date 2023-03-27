dcadd0f323f8f039d20614aafadc5aee738ece3c
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-27 10:26:38 +0530

    fixed types

--- framework/common/types/api.ts
+++ framework/common/types/api.ts
@@ -1,3 +1,5 @@
+import { ApiHooks } from "./hooks"
+
 export type ApiFetcherOptions = {
     query: string
     variables?: Variables
@@ -11,14 +13,7 @@ export type ApiFetcherOptions = {
   export interface ApiConfig {
     fetch<T>(options: ApiFetcherOptions): Promise<ApiFetcherResults<T>>
   }
-
-  export interface ApiHooks {
-    cart: {
-      useAddItem: any
-      useCart: any
-    }
-  }
-
+  
   export type ApiFetcher<T = any> = (
     options: ApiFetcherOptions
   ) => Promise<ApiFetcherResults<T>>
