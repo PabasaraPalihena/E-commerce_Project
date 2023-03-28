d6d55416fb46e6efabaddbea4b24dcb5727498ce
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-28 11:18:32 +0530

    checkout to cart

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -1,5 +1,5 @@
 import useCart from "@common/cart/use-cart"
-import { createCheckout,getCheckoutQuery } from "@framework/utils"
+import { checkoutToCart,createCheckout,getCheckoutQuery } from "@framework/utils"
 import { useMemo } from "react"
 
 export default useCart
@@ -29,7 +29,9 @@ export const handler = {
         checkout = await createCheckout(fetch)
       }
   
-      return checkout
+      const cart = checkoutToCart(checkout)
+      debugger
+      return cart
   },
   useHook: ({useData}: any) => {
     const data = useData({
