e643d29896cd1e9b4a34d3e0cb15a0762f7e2439
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-28 16:39:33 +0530

    handle type remove item

--- framework/shopify/cart/use-remove-item.ts
+++ framework/shopify/cart/use-remove-item.ts
@@ -1,11 +1,12 @@
-import useRemoveItem from "@common/cart/use-remove-item"
+import useRemoveItem, { UseRemoveItem} from "@common/cart/use-remove-item"
 import { Cart } from "@common/types/cart"
 import { MutationHook } from "@common/types/hooks"
 import { CheckoutLineItemsRemovePayload } from "@framework/schema"
 import { checkoutToCart, getCheckoutId } from "@framework/utils"
 import { checkoutLineItemsRemoveMutation } from "@framework/utils/mutations"
 import useCart from "./use-cart"
-export default useRemoveItem
+
+export default useRemoveItem as UseRemoveItem<typeof handler>
 
 export type RemoveItemDescriptor = {
     fetcherInput: {
