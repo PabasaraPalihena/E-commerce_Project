64775ca42b402d11cbca2e0b38441dbc13c334c1
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-28 12:28:05 +0530

    completed the mutation hook type

--- framework/common/cart/use-add-item.ts
+++ framework/common/cart/use-add-item.ts
@@ -1,10 +1,20 @@
-import { useHook, useMutationHook} from "@common/utils/use-hook"
-const useAddItem = () => {
+import { MutationHook } from "@common/types/hooks"
+import {
+  useHook,
+  useMutationHook
+} from "@common/utils/use-hook"
+
+
+export type UseAddItem<
+  H extends MutationHook = MutationHook<any>
+> = ReturnType<H["useHook"]>
+
+const useAddItem: UseAddItem = () => {
   const hook = useHook((hooks) => {
     return hooks.cart.useAddItem
   })
 
-  return useMutationHook({...hook})
+  return useMutationHook({...hook})()
 }
 
 export default useAddItem
--- framework/common/types/hooks.ts
+++ framework/common/types/hooks.ts
@@ -39,5 +39,5 @@ export type MutationHookContext<Input, Output> = {
   >
     useHook(
       context: MutationHookContext<H["fetcherInput"], H["data"]>
-  ): (input: H["fetcherInput"]) => Promise<H["data"]>
+      ): () => (input: H["fetcherInput"]) => Promise<H["data"]>
   }
